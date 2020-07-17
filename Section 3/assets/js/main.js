9e93d26f3042bb35c733682653bcbac28cb84b85
rrezwan7 <write2rezwan@gmail.com>
2020-07-18 03:47:10 +0600

    First Commit

---
+++ Section 3/assets/js/main.js
@@ -0,0 +1,14 @@
+(function ($) {
+    "use strict";
+
+    jQuery(document).ready(function ($) {
+        $(".video-play-btn").magnificPopup({
+            type: 'video'
+        })
+
+    });
+
+    jQuery(window).load(function () {
+
+    });
+}(jQuery));
